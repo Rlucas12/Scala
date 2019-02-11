1b8265c6c244f5e039bbea30d0d01799d390ea50
REYNES Lucas <lucasreynes@gmail.com>
2019-02-11 19:25:50 +0100

    Remove solutions of Sort (BubbleSort, HeapSort, InsertionSort, MergeSort, QuickSort, RecursiveInsertionSort, SelectionSort) :microscope:

--- src/main/scala/Sort/BubbleSort.scala
+++ src/main/scala/Sort/BubbleSort.scala
@@ -10,27 +10,7 @@ object BubbleSort {
     * @return - sequence of sorted integers @array
     */
   def bubbleSort(array: Array[Int]) :  Array[Int] = {
-
-    breakable {
-      for (i <- 0 to array.length - 1) {
-        var swap = false
-
-        for (j <- 0 to array.length - 2) {
-          if (array(j) > array(j + 1)) {
-            val temp = array(j)
-            array(j) = array(j + 1)
-            array(j + 1) = temp
-            swap = true
-          }
-        }
-
-        if (!swap) {
-          break
-        }
-      }
-    }
-
-    array
+    ???
   }
 
 }
--- src/main/scala/Sort/SelectionSort.scala
+++ src/main/scala/Sort/SelectionSort.scala
@@ -8,25 +8,6 @@ object SelectionSort {
     * @return - sequence of sorted integers @array
     */
   def selectionSort(array: Array[Int]): Array[Int] = {
-
-    for(i <- 0 to array.length-1) {
-
-      var min: Int = i
-      var minVal = array(i)
-
-      for(j <- i+1 to array.length-1) {
-        if(array(j) < minVal) {
-          min = j
-          minVal = array(j)
-        }
-      }
-
-      val temp: Int = array(i)
-      array(i)=array(min)
-      array(min)=temp
-
-    }
-
-    array
+    ???
   }
 }
